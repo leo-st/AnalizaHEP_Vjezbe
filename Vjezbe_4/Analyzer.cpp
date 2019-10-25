bef248536210840806525590d806ef227059268e
leo-st <leoivas2103@gmail.com>
2019-10-25 23:15:59 +0200

    kraj4

--- Vjezbe_4/Analyzer.cpp
+++ Vjezbe_4/Analyzer.cpp
@@ -34,14 +34,14 @@ void Analyzer::convertTextToRootFile(){
 	tree->Branch("DecayParticle1_pz",&DecayParticle1_pz);
 	tree->Branch("DecayParticle1_E",&DecayParticle1_E);
 	tree->Branch("DecayParticle1_TransMomentum",&DecayParticle1_TransMomentum);
-	tree->Branch("DecayParticle2_Name",&DecayParticle1_Name);
-	tree->Branch("DecayParticle2_mass",&DecayParticle1_mass);
-	tree->Branch("DecayParticle2_isBoson",&DecayParticle1_isBoson);
-	tree->Branch("DecayParticle2_px",&DecayParticle1_px);
-	tree->Branch("DecayParticle2_py",&DecayParticle1_py);
-	tree->Branch("DecayParticle2_pz",&DecayParticle1_pz);
-	tree->Branch("DecayParticle2_E",&DecayParticle1_E);
-	tree->Branch("DecayParticle2_TransMomentum",&DecayParticle1_TransMomentum);
+	tree->Branch("DecayParticle2_Name",&DecayParticle2_Name);
+	tree->Branch("DecayParticle2_mass",&DecayParticle2_mass);
+	tree->Branch("DecayParticle2_isBoson",&DecayParticle2_isBoson);
+	tree->Branch("DecayParticle2_px",&DecayParticle2_px);
+	tree->Branch("DecayParticle2_py",&DecayParticle2_py);
+	tree->Branch("DecayParticle2_pz",&DecayParticle2_pz);
+	tree->Branch("DecayParticle2_E",&DecayParticle2_E);
+	tree->Branch("DecayParticle2_TransMomentum",&DecayParticle2_TransMomentum);
 	
 
 
