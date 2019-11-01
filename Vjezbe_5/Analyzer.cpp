7b485ab748f13ed06236ab295f61fcf79850eea4
leo-st <leoivas2103@gmail.com>
2019-11-01 12:07:43 +0100

    gotovo 5.vjezbe

--- Vjezbe_5/Analyzer.cpp
+++ Vjezbe_5/Analyzer.cpp
@@ -4,12 +4,17 @@
 #include <TStyle.h>
 #include <TCanvas.h>
 #include <TLegend.h>
+#include <TLorentzVector.h>
+#include <cmath>
 
 void Analyzer::Loop()
 {	
+	TLorentzVector* v1=new TLorentzVector();
 	TCanvas *c= new TCanvas("c","c",900,900);
+	c->Divide(2,1);
 	TH1F *h1= new TH1F("DecayParticle1_TransMomentum", "naslov",50,0,100);
 	TH1F *h2= new TH1F("DecayParticle2_TransMomentum", "naslov",50,0,100);
+	TH1F *h3= new TH1F("Higgs_TransMomentum", "Higgs_TransMomentum",50,0,200);
 	h1->SetStats(0);
 	h2->SetStats(0);
    if (fChain == 0) return;
@@ -24,7 +29,10 @@ void Analyzer::Loop()
       // if (Cut(ientry) < 0) continue;
 	h1->Fill(DecayParticle1_TransMomentum);
 	h2->Fill(DecayParticle2_TransMomentum);
+	v1->SetPxPyPzE(DecayParticle1_px+DecayParticle2_px,DecayParticle1_py+DecayParticle2_py,DecayParticle1_pz+DecayParticle2_pz,sqrt(pow(DecayParticle1_px+DecayParticle2_px,2)+pow(DecayParticle1_py+DecayParticle2_py,2)+pow(DecayParticle1_pz+DecayParticle2_pz,2)+pow(DecayParticle1_mass+DecayParticle2_mass,2)));
+	h3->Fill(sqrt(v1->Px()*v1->Px()+v1->Py()*v1->Py()));
    }
+   c->cd(1);
    h1->GetXaxis()->SetTitle("Transverzalna kolicina gibanja");
    h1->GetYaxis()->SetTitle("Broj dogadaja");
    h1->SetLineColor(kRed);
@@ -42,7 +50,8 @@ void Analyzer::Loop()
    t1->AddEntry(h1, "transverzalna kolicina gibanja 1");
    t1->AddEntry(h2, "transverzalna kolicina gibanja 2");
    t1->Draw();
-   
+   c->cd(2);
+   h3->Draw();
 
    c->SaveAs("test.pdf");
 }
