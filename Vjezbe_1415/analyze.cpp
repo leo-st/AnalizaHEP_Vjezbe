272beeb2ae61ad61a5c6cb2d8e692a157e663841
leo-st <leoivas2103@gmail.com>
2020-01-24 12:01:47 +0100

    zadnje vjezbe

---
+++ Vjezbe_1415/analyze.cpp
@@ -0,0 +1,14 @@
+#include "Analyzer.h"
+
+using namespace std;
+
+int main(){
+
+	Analyzer *a = new Analyzer();
+	//a->Loop();
+	a->Theoretical_noHiggs();
+	a->Expected_p_value_scan();
+	a->PlotMass();
+	
+return 0;
+}
